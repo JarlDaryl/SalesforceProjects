dd75d7a0546cd002f36632bd5486bcc5070bbe60
Luis <luisarquescalero@gmail.com>
2024-12-10 12:31:40 +0100

    Updating project

--- lwcForVisualforceDevs/force-app/main/default/lwc/controls/controls.js
+++ lwcForVisualforceDevs/force-app/main/default/lwc/controls/controls.js
@@ -7,4 +7,11 @@ export default class Controls extends LightningElement {
       handleSubtract() {
         this.dispatchEvent(new CustomEvent('subtract'));
       }
+      handleMultiply(event) {
+        const factor = event.target.dataset.factor;
+        this.dispatchEvent(new CustomEvent('multiply', {
+          detail: factor
+        }));
+      }
+      factors = [0,2,3,4,5,6];
 }
--- lwcForVisualforceDevs/force-app/main/default/lwc/numerator/numerator.js
+++ lwcForVisualforceDevs/force-app/main/default/lwc/numerator/numerator.js
@@ -1,11 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-    counter = 0;
+    @api counter = 0;
     handleIncrement() {
       this.counter++;
     }
     handleDecrement() {
       this.counter--;
+    }
+    handleMultiply(event) {
+      const factor = event.detail;
+      this.counter *= factor;
+    }
+    @api
+    maximizeCounter() {
+      this.counter += 1000000;
     }
 }
